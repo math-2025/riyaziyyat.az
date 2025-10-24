0a0c74b04a90036ee25a712e85cb2e933b4eac0f
math-2025 <physicswithme.2026@gmail.com>
2025-10-24 13:51:03 +0400

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
-"use client";
-
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,11 +10,8 @@ const firebaseConfig = {
   projectId: "studio-461864257-d9b57",
   storageBucket: "studio-461864257-d9b57.firebasestorage.app",
   messagingSenderId: "952093304826",
-  appId: "1:952093304826:web:c208c1c171f524fac1e643"
+  appId: "1:952093304826:web:c22c8644eabf2803c1e643"
 };
 
-
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-export { app };
+const app = initializeApp(firebaseConfig);
